b9174606d5eefcceb6552cbcf5fad31f67b313fb
taeyun1010 <taeyun1010@yahoo.ca>
2018-03-16 15:45:18 +0900

    Add files via upload

--- addBit.m
+++ addBit.m
@@ -2,20 +2,24 @@ function [suma,sumb,carrya,carryb] = addBit(input1a,input1b, input2a, input2b, i
 %ADDBIT Summary of this function goes here
 %   Detailed explanation goes here
     
+%tic
+
 % input1 XOR input2
-[1xor2a, 1xor2b] = xorMat(input1a, input1b, input2a, input2b);
+[onexor2a, onexor2b] = xorMat(input1a, input1b, input2a, input2b);
 
 % sum, without carry
-[suma, sumb] = xorMat(1xor2a, 1xor2b, inputcarrya, inputcarryb);
+[suma, sumb] = xorMat(onexor2a, onexor2b, inputcarrya, inputcarryb);
 
 % input1 AND input2
-[1and2a, 1and2b] = andMat(input1a, input1b, input2a, input2b);
+[oneand2a, oneand2b] = andMat(input1a, input1b, input2a, input2b);
 
 % (input1 xor input2) and inputcarry
-[intermediatea, intermediateb] = andMat(1xor2a, 1xor2b, inputcarrya, inputcarryb);
+[intermediatea, intermediateb] = andMat(onexor2a, onexor2b, inputcarrya, inputcarryb);
 
 % carry
-[carrya, carryb] = orMat(1and2a, 1and2b, intermediatea, intermediateb);
+[carrya, carryb] = orMat(oneand2a, oneand2b, intermediatea, intermediateb);
+
+%toc
 
 end
 
